bf3263c7467d23c6611bffd1d189f187c6d3c667
adityjha0 <aditya2005aps@gmail.com>
2023-12-15 20:07:17 +0530

    using a struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "raylib.h"
 
+struct animdata{
+    Rectangle rec;
+    Vector2 pos;
+    int frame;
+    float updatetime,runningtime;
+};
+
+
 int main(){
     const int width = 512, height = 380;
     InitWindow(width,height,"Dapper-Dasher!");
@@ -11,29 +19,15 @@ int main(){
     int velocity = 0;
     
     Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png");
-    Rectangle nebularec{0.0, 0.0, nebula.width/8, nebula.height/8};
-    Vector2 nebulapos{width,height-nebularec.height};
+    animdata nebdata{{0.0,0.0,nebula.width/8.0,nebula.height/8.0},{width,height/1.0-nebula.height/8},0,1.0/12.0,0.0};
 
     int nebulavel{-500};
 
     Texture2D scarfy = LoadTexture("textures/scarfy.png");
-    Rectangle scarfyrec;
-    scarfyrec.width=scarfy.width/6;
-    scarfyrec.height=scarfy.height;
-    scarfyrec.x=0;
-    scarfyrec.y=0;
+    animdata scarfydata{{0.0,0.0,scarfy.width/6.0,scarfy.height/1.0},{width/5,height/1.0-scarfy.height},0,1.0/12.0,0.0};
 
-    Vector2 scarfypos;
-    scarfypos.x=(width-scarfyrec.width)/2;
-    scarfypos.y=height-scarfyrec.height;
 
-    int frame{0};
-    const float updatetime= 1.0/12.0;
-    float runningtime{};
 
-    int nebframe{0};
-    const float nebupdatetime= 1.0/12.0;
-    float nebrunningtime{};
 
     SetTargetFPS(60);
     while (!WindowShouldClose()){
@@ -43,9 +37,9 @@ int main(){
         const float dt = GetFrameTime();
 
 
-        if(scarfypos.y>=height-scarfyrec.width){
+        if(scarfydata.pos.y>=height-scarfydata.rec.width){
             velocity=0;
-            scarfypos.y=height-scarfyrec.width;
+            scarfydata.pos.y=height-scarfydata.rec.width;
             isinair=false;
 
         }else{
@@ -57,30 +51,30 @@ int main(){
             
         }
         
-        nebulapos.x += nebulavel*dt;
+        nebdata.pos.x += nebulavel*dt;
 
-        scarfypos.y+=velocity*dt;
+        scarfydata.pos.y+=velocity*dt;
         
-        runningtime+=dt;
-        if(runningtime>=updatetime and !isinair){
-            runningtime=0.0;
-            scarfyrec.x = frame*scarfyrec.width;
-            frame++;
-            frame = frame%6;
+        scarfydata.runningtime+=dt;
+        if(scarfydata.runningtime>=scarfydata.updatetime and !isinair){
+            scarfydata.runningtime=0.0;
+            scarfydata.rec.x = scarfydata.frame*scarfydata.rec.width;
+            scarfydata.frame++;
+            scarfydata.frame = scarfydata.frame%6;
         }
 
 
-        nebrunningtime+=dt;
-        if(nebrunningtime>=nebupdatetime){
-            nebrunningtime=0.0;
-            nebularec.x = nebframe*nebularec.width;
-            nebframe++;
-            nebframe = nebframe%8;
+        nebdata.runningtime+=dt;
+        if(nebdata.runningtime>=nebdata.updatetime){
+            nebdata.runningtime=0.0;
+            nebdata.rec.x = nebdata.frame*nebdata.rec.width;
+            nebdata.frame++;
+            nebdata.frame = nebdata.frame%8;
         }
 
-        DrawTextureRec(nebula,nebularec,nebulapos,WHITE);
+        DrawTextureRec(nebula,nebdata.rec,nebdata.pos,WHITE);
         
-        DrawTextureRec(scarfy,scarfyrec,scarfypos,WHITE);
+        DrawTextureRec(scarfy,scarfydata.rec,scarfydata.pos,WHITE);
         
         EndDrawing();
     }
